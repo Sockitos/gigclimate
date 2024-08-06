d0f94e1656abc667be06f52ec446b2a049ca743b
shuhaomadesign <shuhao.ma@tecnico.ulisboa.pt>
2024-08-06 14:00:33 +0100

    change the selected location

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -241,14 +241,11 @@
 				{#if selectedLocation}
 					<Marker latLng={selectedLocation}>
 						<Icon
-							iconUrl="https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png"
+							iconUrl="https://mashuhao.me/wp-content/uploads/2024/08/selected_location.png"
 							options={{
-								iconSize: [25, 41],
-								iconAnchor: [12, 41],
+								iconSize: [29, 41],
+								iconAnchor: [14, 41],
 								popupAnchor: [1, -34],
-								shadowUrl:
-									'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-								shadowSize: [41, 41],
 								zIndexOffset: 2000
 							}}
 						/>
